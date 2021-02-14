01901066169de9fe093eff8b017274714a0012d1
rhyanfauzan <rhyananiki@gmail.com>
2021-02-14 22:04:54 +0700

    first

---
+++ app/src/main/java/com/ryandev/aplikasidesa/MainActivity.java
@@ -0,0 +1,45 @@
+package com.ryandev.aplikasidesa;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            Fragment f =null;
+            switch (item.getItemId()){
+                case R.id.menu_home:
+                    f = new FragmentHome();
+                    break;
+                case R.id.menu_info:
+                    f = new FragmentInfo();
+                    break;
+                case R.id.menu_notif:
+                    f = new FragmentNotif();
+                    break;
+                case R.id.menu_profile:
+                    f = new FragmentProfile();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
+    }
+}
