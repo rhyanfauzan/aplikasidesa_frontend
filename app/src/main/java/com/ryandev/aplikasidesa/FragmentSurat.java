bf9653fb0298a9f55f3b6abfe8659d361643f88c
rhyanfauzan <rhyananiki@gmail.com>
2021-02-16 17:55:14 +0700

    ScrollView update 1.5

--- app/src/main/java/com/ryandev/aplikasidesa/FragmentSurat.java
+++ app/src/main/java/com/ryandev/aplikasidesa/FragmentSurat.java
@@ -4,15 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentSurat extends Fragment {
+public class FragmentSurat extends Fragment implements AdapterView.OnItemSelectedListener {
+    Spinner aSpinner;
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_surat, container, false);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
